2ff95b776ea357dda937941b72bf9c3733041bec
jusun <nmming@naver.com>
2020-06-11 18:05:49 +0900

    first commit

---
+++ src/main/java/com/jusun/first/domins/PokemonDTO.java
@@ -0,0 +1,10 @@
+package com.jusun.first.domins;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class PokemonDTO {
+   public String name,stamina,atk,def,captureRate,fleeRate,spawnChance,prim,secondary,cp,url;
+}
